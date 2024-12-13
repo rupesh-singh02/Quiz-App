8b1936ce2a255a27e7b2a487ceec04ded4a86089
Rupesh Singh <rupesh.singhghy@gmail.com>
2024-12-13 16:02:04 +0530

    Allowed Host

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_8cc$bru+lp9)mjpoec8p=$r+)6dlp&1ol44@9wnc7v&2clcsg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
